1c97aa3e2fae650bb236ee6febe563ed4e86b0c5
Eric Dobbertin <eric@dairystatedesigns.com>
2013-07-22 16:16:22 -0500

    fix load order

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
-  summary: "A smart package for Meteor that adds handlebars helpers to easily create basic forms with automatic insert and update events, and automatic reactive validation."
+    summary: "A smart package for Meteor that adds handlebars helpers to easily create basic forms with automatic insert and update events, and automatic reactive validation."
 });
 
 Package.on_use(function(api) {
-  api.use(['underscore', 'templating']);
-  api.add_files(['autoform.html', 'autoform.js'], ['client']);
+    api.use('underscore', ['client', 'server']);
+    api.use('deps', 'client');
+    api.use('startup', 'client');
+    api.use('templating', 'client');
+    api.use('collection2', ['client', 'server']);
+
+    api.add_files(['autoform.html', 'autoform.js'], ['client']);
 });
