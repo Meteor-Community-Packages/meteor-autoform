c656de19557489fcbc64b9cb5c89921a6957556c
Eric Dobbertin <eric@dairystatedesigns.com>
2013-08-20 11:29:15 -0500

    use deps on server

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function(api) {
     api.use('underscore', ['client', 'server']);
-    api.use('deps', 'client');
+    api.use('deps', ['client', 'server']);
     api.use('templating', 'client');
     api.use('handlebars', 'client');
     api.use('collection2', ['client', 'server'], {weak: true});
