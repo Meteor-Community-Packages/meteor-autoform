9e3ed6e5a7d2df1be97bfca0baede5d1672070fe
Raphael Stäbler <rs@instanz.ch>
2014-02-04 08:31:01 +0100

    fixed line indenting

--- autoform-client.js
+++ autoform-client.js
@@ -460,18 +460,18 @@ if (typeof Handlebars !== 'undefined') {
       var ss = afObj.simpleSchema();
       var resetOnSuccess = template.data._resetOnSuccess;
 
-	  // Gather hooks, falling back to the deprecated API
-	  // TODO eventually remove support for old API
-	  var beforeInsert = hooks.before.insert || afObj.beforeInsert;
-	  var beforeUpdate = hooks.before.update || afObj.beforeUpdate;
-	  var beforeRemove = hooks.before.remove || afObj.beforeRemove;
-	  var beforeMethod = method && (hooks.before[method] || afObj.beforeMethod);
-	  var afterInsert = hooks.after.insert || afObj._callbacks.insert;
-	  var afterUpdate = hooks.after.update || afObj._callbacks.update;
-	  var afterRemove = hooks.after.remove || afObj._callbacks.remove;
-	  var afterMethod = method && (hooks.after[method] || afObj._callbacks[method]);
-	  var onSuccess = hooks.onSuccess;
-	  var onError = hooks.onError;
+      // Gather hooks, falling back to the deprecated API
+      // TODO eventually remove support for old API
+      var beforeInsert = hooks.before.insert || afObj.beforeInsert;
+      var beforeUpdate = hooks.before.update || afObj.beforeUpdate;
+      var beforeRemove = hooks.before.remove || afObj.beforeRemove;
+      var beforeMethod = method && (hooks.before[method] || afObj.beforeMethod);
+      var afterInsert = hooks.after.insert || afObj._callbacks.insert;
+      var afterUpdate = hooks.after.update || afObj._callbacks.update;
+      var afterRemove = hooks.after.remove || afObj._callbacks.remove;
+      var afterMethod = method && (hooks.after[method] || afObj._callbacks[method]);
+      var onSuccess = hooks.onSuccess;
+      var onError = hooks.onError;
 
       if (isInsert || isUpdate || isRemove || method) {
         event.preventDefault(); //prevent default here if we're planning to do our own thing
@@ -479,15 +479,15 @@ if (typeof Handlebars !== 'undefined') {
 
       var form = formValues(template, hooks.formToDoc || afObj.formToDoc, ss);
 
-	  var validationErrorTriggered = 0;
-	  function isValid(doc) {
-		  var result = validationType === 'none' || ss.namedContext(formId).validate(doc, {modifier: false});
-		  if (!result && !validationErrorTriggered) {
-			  onError && onError('validation', new Error('failed validation'), template);
-			  validationErrorTriggered++;
-		  }
-		  return result;
-	  }
+      var validationErrorTriggered = 0;
+      function isValid(doc) {
+        var result = validationType === 'none' || ss.namedContext(formId).validate(doc, {modifier: false});
+        if (!result && !validationErrorTriggered) {
+          onError && onError('validation', new Error('failed validation'), template);
+          validationErrorTriggered++;
+        }
+        return result;
+      }
 
       //pass both types of doc to onSubmit
       if (hasOnSubmit) {
