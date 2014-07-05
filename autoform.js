7e696a2c848ea50924323a638aa6d8d2a9b5ec22
Eric Dobbertin <eric@dairystatedesigns.com>
2014-07-05 16:22:29 -0500

    fix #250; respect type attribute for array and object fields

--- autoform.js
+++ autoform.js
@@ -384,15 +384,16 @@ Template.afQuickField.innerContext = function afQuickFieldInnerContext(options) 
 
 Template.afQuickField.isGroup = function afQuickFieldIsGroup(options) {
   var c = Utility.normalizeContext(options.hash, "afQuickField");
-  // Render a group of fields if we expect an Object
-  return (c.defs.type === Object);
+  // Render an array of fields if we expect an Object and we don't have options
+  // and we have not overridden the type
+  return (c.defs.type === Object && !c.atts.options && !c.atts.type);
 };
 
 Template.afQuickField.isFieldArray = function afQuickFieldIsFieldArray(options) {
   var c = Utility.normalizeContext(options.hash, "afQuickField");
-
   // Render an array of fields if we expect an Array and we don't have options
-  return (c.defs.type === Array && !c.atts.options);
+  // and we have not overridden the type
+  return (c.defs.type === Array && !c.atts.options && !c.atts.type);
 };
 
 /*
