dc504e416f549d8be1eda66923e6cca62447521a
Eric Dobbertin <eric@dairystatedesigns.com>
2014-11-18 11:12:40 -0600

    add value converters for hidden type; fixes #453

--- inputTypes/hidden/hidden.js
+++ inputTypes/hidden/hidden.js
@@ -1,3 +1,50 @@
 AutoForm.addInputType("hidden", {
-  template: "afInputHidden"
+  template: "afInputHidden",
+  valueOut: function () {
+    return this.val();
+  },
+  valueConverters: {
+    "stringArray": function (val) {
+      if (typeof val === "string") {
+        val = val.split(",");
+        return _.map(val, function (item) {
+          return $.trim(item);
+        });
+      }
+      return val;
+    },
+    "number": AutoForm.Utility.stringToNumber,
+    "numberArray": function (val) {
+      if (typeof val === "string") {
+        val = val.split(",");
+        return _.map(val, function (item) {
+          item = $.trim(item);
+          return AutoForm.Utility.stringToNumber(item);
+        });
+      }
+      return val;
+    },
+    "boolean": AutoForm.Utility.stringToBool,
+    "booleanArray": function (val) {
+      if (typeof val === "string") {
+        val = val.split(",");
+        return _.map(val, function (item) {
+          item = $.trim(item);
+          return AutoForm.Utility.stringToBool(item);
+        });
+      }
+      return val;
+    },
+    "date": AutoForm.Utility.stringToDate,
+    "dateArray": function (val) {
+      if (typeof val === "string") {
+        val = val.split(",");
+        return _.map(val, function (item) {
+          item = $.trim(item);
+          return AutoForm.Utility.stringToDate(item);
+        });
+      }
+      return val;
+    }
+  }
 });
