fe038f3afb7c02d95be3da87448a606aff302038
Eric Dobbertin <eric@dairystatedesigns.com>
2014-11-18 14:32:47 -0600

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "aldeed:autoform",
   summary: "Easily create forms with automatic insert and update, and automatic reactive validation.",
   git: "https://github.com/aldeed/meteor-autoform.git",
-  version: "4.0.3"
+  version: "4.0.4"
 });
 
 Package.on_use(function(api) {
