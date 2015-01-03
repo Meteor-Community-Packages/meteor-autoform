25f152623b4ecfdc995c5a5428667d7a523531bf
comerc <comerc@mail.ru>
2015-01-03 18:05:06 +0300

    contenteditable placeholder

--- inputTypes/contenteditable/contenteditable.js
+++ inputTypes/contenteditable/contenteditable.js
@@ -4,15 +4,24 @@ AutoForm.addInputType("contenteditable", {
     return this.html();
   },
   contextAdjust: function (context) {
-    if (typeof context.atts['data-maxlength'] === "undefined" && typeof context.max === "number") {
-      context.atts['data-maxlength'] = context.max;
+    if (typeof context.atts["data-maxlength"] === "undefined" && typeof context.max === "number") {
+      context.atts["data-maxlength"] = context.max;
+    }
+    if (context.atts["placeholder"]) {
+      context.atts["data-placeholder"] = context.atts["placeholder"];
+      delete context.atts["placeholder"];
     }
     return context;
   }
 });
 
 Template.afContenteditable.events({
-  'blur div[contenteditable=true]': function (event, template) {
-    template.$(event.target).change();
-  }
+  "blur [contenteditable]": function (event, template) {
+    // placeholder issue: http://stackoverflow.com/a/27755631/4315996
+    var $element = template.$(event.target);
+    if ($element.html() && !$element.text().trim().length) {
+        $element.empty();
+    }
+    $element.change();
+  },
 });
