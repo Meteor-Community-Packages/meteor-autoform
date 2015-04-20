26e96ffe2e4c8f56d50f3d49f402e281c5966100
Sacha Greif <sacha357@gmail.com>
2015-04-20 10:33:10 +0900

    if atts.fields is specified, restrict and reorder schema; handle group.$.field and group.0.field arguments

--- components/quickForm/quickForm.js
+++ components/quickForm/quickForm.js
@@ -5,6 +5,7 @@ Template.quickForm.helpers({
     return AutoForm.getTemplateName('quickForm', this.template);
   },
   innerContext: function quickFormContext() {
+
     var atts = this;
     var schema = {};
     var fieldGroups = []; 
@@ -12,6 +13,7 @@ Template.quickForm.helpers({
     // --------------- A. Schema --------------- //
     // get schema
     // note: replace this by a standard function?
+
     if (atts.schema && atts.schema._schema) {
       schema = atts.schema._schema;
     } else if (atts.collection) {
@@ -21,14 +23,19 @@ Template.quickForm.helpers({
         schema = atts.collection.simpleSchema()._schema;
       }
     }
-    
+
     if (atts.fields) {
       // if atts.fields exists, transform it into an array
       // note: is there a standard function we can use here instead of replace and split?
       atts.fields = atts.fields.replace(" ", "").split(",");
 
-      // then restrict the schema to specified fields
-      // note: haven't found a clean way to do this all in one go yet, so doing it later in B & C. 
+      // then loop over atts.fields to restrict the schema to its fields and reorder it
+      var sortedSchema = {};
+      atts.fields.forEach(function (fieldName) {
+        var genericFieldName = SimpleSchema._makeGeneric(fieldName); // use "$"" instead of "0"
+        sortedSchema[fieldName] = schema[genericFieldName];
+      });
+      schema = sortedSchema;
     }
 
     // --------------- B. Field With No Groups --------------- //
@@ -37,11 +44,8 @@ Template.quickForm.helpers({
     var defaultGroup = {name: '_defaultGroup', atts: _.clone(atts)};
 
     // get all fields with no field group specified
-    // always omit fields with "$" in their name
-    // note: if atts.fields is specified, only consider fields contained in it
-    defaultGroup.atts.fields = _.compact(_.map(schema, function (property, key) {
-      key = SimpleSchema._makeGeneric(key);
-      return key.indexOf("$") == "-1" && (!atts.fields || _.contains(atts.fields, key)) && (!property.autoform || !property.autoform.group) && key;
+    defaultGroup.atts.fields = _.compact(_.map(schema, function (field, fieldName) {
+      return (!field.autoform || !field.autoform.group) && fieldName;
     }));;
 
     fieldGroups.push(defaultGroup);
@@ -49,17 +53,15 @@ Template.quickForm.helpers({
     // --------------- C. Field With Groups --------------- //
 
     // get list of unique field groups with any falsy values removed
-    // note: if atts.fields is specified, only consider fields contained in it
-    var fieldGroupNames = _.compact(_.unique(_.map(schema, function (property, key) {
-      return (!atts.fields || _.contains(atts.fields, key)) && property.autoform && property.autoform.group ;
+    var fieldGroupNames = _.compact(_.unique(_.map(schema, function (field, fieldName) {
+      return field.autoform && field.autoform.group ;
     }))).sort();
 
     // loop over field group names, and push relevant field to fieldGroups array
-    _.each(fieldGroupNames, function (fieldGroupName, key) {
-
+    _.each(fieldGroupNames, function (fieldGroupName) {
       // for each field group, get list of field names
-      var fieldsForGroup = _.compact(_.map(schema, function (property, key) {
-        return property.autoform && property.autoform.group && property.autoform.group === fieldGroupName && key;
+      var fieldsForGroup = _.compact(_.map(schema, function (field, fieldName) {
+        return field.autoform && field.autoform.group && field.autoform.group === fieldGroupName && fieldName;
       }));
 
       // copy parent atts over to field group context, while adding "name" and overwriting "fields"
@@ -88,6 +90,7 @@ Template.quickForm.helpers({
       qfShouldRenderButton: qfShouldRenderButton,
       fieldGroups: fieldGroups
     };
+
     return context;
   }
 });
