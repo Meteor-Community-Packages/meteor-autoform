dbaccdd76c9ffcba6ca44ad589c5e8a6f547b05f
Franz Knipp <franz@qnipp.com>
2015-05-13 15:56:17 +0200

    * afObjectField: Provide autoform attributes to the templates * Themes bootstrap3, bootstrap3-horizontal: Set additional classes on div.panel, div.panel-heading and div.panel-body through the attributes panelClass, headingClass and bodyClass

--- components/afObjectField/afObjectField.js
+++ components/afObjectField/afObjectField.js
@@ -3,5 +3,10 @@
 Template.afObjectField.helpers({
   getTemplateName: function () {
     return AutoForm.getTemplateName('afObjectField', this.template, this.name);
+  },
+  innerContext: function() {
+    var c = AutoForm.Utility.getComponentContext(this, 'afObjectField');
+    _.extend(this, c.atts);
+    return this;
   }
 });
--- templates/bootstrap3/components/afObjectField/afObjectField.js
+++ templates/bootstrap3/components/afObjectField/afObjectField.js
@@ -1,5 +1,8 @@
 Template.afObjectField_bootstrap3.helpers({
   quickFieldsAtts: function () {
     return _.pick(this, 'name', 'id-prefix');
+  },
+  panelClass: function() {
+    return this.panelClass || 'panel-default';
   }
 });
