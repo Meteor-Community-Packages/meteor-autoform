faa46fe855d15315a6d6ec417e02c4e1b5cf0b29
Patrick Scott <patrick.lee.scott@gmail.com>
2015-05-15 01:08:29 -0400

    add ability to specify a ddp source for a meteor method, for use with remote ddp sources such as microservices

--- formTypes/method-update.js
+++ formTypes/method-update.js
@@ -27,7 +27,14 @@ AutoForm.addFormType('method-update', {
         c.failedValidation();
       } else {
         // Call the method
-        Meteor.call(c.formAttributes.meteormethod, updateDoc, c.docId, c.result);
+        // if a ddp connection was provided, use that instead of the default
+        // Meteor connection
+        var ddp = c.formAttributes.ddp;
+        if (ddp && ddp.call && typeof ddp.call === 'function') {
+          ddp.call(c.formAttributes.meteormethod, updateDoc, c.docId, c.result);
+        } else {
+          Meteor.call(c.formAttributes.meteormethod, updateDoc, c.docId, c.result);
+        }
       }
     });
   },
