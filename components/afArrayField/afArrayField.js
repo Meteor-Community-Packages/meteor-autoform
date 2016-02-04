98e2bd88c62d8ed58fb96ca4220cc601a48b6ac4
Or Rikon <rikonor@gmail.com>
2016-02-04 15:04:04 -0500

    Added enableSortable option for arrays

--- components/afArrayField/afArrayField.js
+++ components/afArrayField/afArrayField.js
@@ -6,6 +6,9 @@ Template.afArrayField.helpers({
   },
   innerContext: function afArrayFieldContext() {
     var c = AutoForm.Utility.getComponentContext(this, "afArrayField");
+
+    this.enableSorting = !! c.atts.enableSorting;
+
     var name = c.atts.name;
     var fieldMinCount = c.atts.minCount || 0;
     var fieldMaxCount = c.atts.maxCount || Infinity;
@@ -24,3 +27,44 @@ Template.afArrayField.helpers({
     };
   }
 });
+
+Template.afArrayField.rendered = function() {
+  var self = this;
+
+  function onSortUpdate() {
+    var arrayItems = self.findAll(".autoform-array-item");
+    _.each(arrayItems, function(arrayItem, i) {
+      function fixPosition(el) {
+        var dataSchemaKey = $(el).attr("data-schema-key");
+
+        if (! dataSchemaKey) return;
+        var schemaFields = dataSchemaKey.split('.');
+
+        var mainField = schemaFields[0];
+        var subField = schemaFields[2];
+
+        dataSchemaKey = mainField + '.' + i + '.' + subField;
+
+        $(el).attr("data-schema-key", dataSchemaKey);
+        $(el).attr("name", dataSchemaKey);
+      }
+
+      _.each($(arrayItem).find("input"), fixPosition);
+      _.each($(arrayItem).find("select"), fixPosition);
+    });
+  }
+
+  if (!! this.data.enableSorting) {
+    var listGroup = $(this.find(".list-group"));
+
+    if (! listGroup.sortable) {
+      throw new Error("Sortable arrays require jQuery UI");
+    }
+
+    listGroup.sortable({
+      handle: ".autoform-drag-item",
+      cancel: ".autoform-add-item-wrap",
+      update: onSortUpdate
+    });
+  }
+};
