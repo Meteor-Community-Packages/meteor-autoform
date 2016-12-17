f5d569e06509ab7f3838a35955d744bb6f806dfc
cesarve77 <c@imagenproactiva.com>
2016-12-17 14:39:52 +1000

    156 - 163

--- templates/bootstrap3/components/afObjectField/afObjectField.js
+++ templates/bootstrap3/components/afObjectField/afObjectField.js
@@ -1,8 +1,10 @@
 Template.afObjectField_bootstrap3.helpers({
   quickFieldsAtts: function () {
+    console.log(this,Template.instance())
     return _.pick(this, 'name', 'id-prefix');
   },
   panelClass: function() {
+    console.log(this,Template.instance())
     return this.panelClass || 'panel-default';
   }
 });
