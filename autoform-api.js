dce3f13e29c46b836e5a440a65bcbb64692774bb
Pouya Eghbali <persian.writer@gmail.com>
2019-11-20 17:12:19 +0100

    Fix AutoForm._inputTypeDefinitions issue

--- autoform-api.js
+++ autoform-api.js
@@ -587,7 +587,7 @@ AutoForm.getInputValue = function autoFormGetInputValue(element, ss) {
   }
 
   // Figure out what registered input type was used to render this element
-  typeDef = AutoForm._inputTypeDefinitions.filter(def => def.template == inputTypeTemplate)[0];
+  typeDef = Object.values(AutoForm._inputTypeDefinitions).filter(def => def.template == inputTypeTemplate)[0];
 
   // If field has a "data-null-value" attribute, value should always be null
   if (field.attr('data-null-value') !== void 0) {
