6b1010b52e1c6babfacd705ea3b568338d7191c5
Pouya Eghbali <persian.writer@gmail.com>
2019-11-22 14:26:09 +0100

    Modify isObject to return true for functions and arrays (to match underscore behavior)

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-export const isObject = obj => toString.call(obj) == '[object Object]'
+export const isObject = obj => toString.call(obj) == '[object Object]' || Array.isArray(obj) || isFunction(obj)
 export const isFunction = obj => toString.call(obj) == '[object Function]'
 
 export const throttle = (fn, limit) => {
