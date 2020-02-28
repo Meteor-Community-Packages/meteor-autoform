012af0cf27c7534f8bed886c78ef51d98261d342
Pouya Eghbali <pouya.eghbali@yandex.com>
2020-02-28 14:54:18 +0100

    Export getInnerContext function

--- components/afFieldInput/afFieldInput.js
+++ components/afFieldInput/afFieldInput.js
@@ -3,8 +3,75 @@
 Template.afFieldInput.onRendered(() => {
   const template = AutoForm.templateInstanceForForm();
   const instance = Template.instance();
-  updateTrackedFieldValue(template, instance.afFieldName, instance.afFieldValue);
-})
+  updateTrackedFieldValue(
+    template,
+    instance.afFieldName,
+    instance.afFieldValue
+  );
+});
+
+export function afFieldInputContext() {
+  var c = AutoForm.Utility.getComponentContext(this, "afFieldInput");
+  var form = AutoForm.getCurrentDataForForm();
+  var formId = form.id;
+  var ss = AutoForm.getFormSchema();
+  var defs = c.defs;
+  const instance = Template.instance();
+  const template = AutoForm.templateInstanceForForm();
+
+  // Get schema default value.
+  // We must do this before adjusting defs for arrays.
+  var schemaDefaultValue = defs.defaultValue;
+
+  // Adjust for array fields if necessary
+  if (defs.type === Array) {
+    defs = AutoForm.Utility.getFieldDefinition(ss, c.atts.name + ".$");
+  }
+
+  // Determine what `type` attribute should be if not set
+  var inputType = AutoForm.getInputType(this);
+  var componentDef = AutoForm._inputTypeDefinitions[inputType];
+  if (!componentDef) {
+    throw new Error(
+      'AutoForm: No component found for rendering input with type "' +
+        inputType +
+        '"'
+    );
+  }
+
+  // Get reactive mDoc
+  var mDoc = AutoForm.reactiveFormData.sourceDoc(formId);
+
+  // Get input value
+  var value = getInputValue(
+    c.atts,
+    c.atts.value,
+    mDoc,
+    schemaDefaultValue,
+    c.atts.defaultValue,
+    componentDef,
+    template
+  );
+
+  // Build input data context
+  var iData = getInputData(
+    defs,
+    c.atts,
+    value,
+    ss.label(c.atts.name),
+    form.type
+  );
+
+  // These are needed for onRendered
+
+  instance.afFieldName = c.atts.name;
+  instance.afFieldValue = value;
+
+  // Adjust and return context
+  return typeof componentDef.contextAdjust === "function"
+    ? componentDef.contextAdjust(iData)
+    : iData;
+}
 
 Template.afFieldInput.helpers({
   // similar to AutoForm.getTemplateName, but we have fewer layers of fallback, and we fall back
@@ -16,7 +83,11 @@ Template.afFieldInput.helpers({
     var inputType = AutoForm.getInputType(this);
     var componentDef = AutoForm._inputTypeDefinitions[inputType];
     if (!componentDef) {
-      throw new Error('AutoForm: No component found for rendering input with type "' + inputType + '"');
+      throw new Error(
+        'AutoForm: No component found for rendering input with type "' +
+          inputType +
+          '"'
+      );
     }
 
     var inputTemplateName = componentDef.template;
@@ -24,11 +95,16 @@ Template.afFieldInput.helpers({
 
     // We skip the check for existence here so that we can get the `_plain` string
     // even though they don't exist.
-    var templateName = AutoForm.getTemplateName(inputTemplateName, styleTemplateName, self.name, true);
+    var templateName = AutoForm.getTemplateName(
+      inputTemplateName,
+      styleTemplateName,
+      self.name,
+      true
+    );
 
     // Special case: the built-in "plain" template uses the basic input templates for
     // everything, so if we found _plain, we use inputTemplateName instead
-    if (templateName.indexOf('_plain') !== -1) {
+    if (templateName.indexOf("_plain") !== -1) {
       templateName = null;
     }
 
@@ -39,46 +115,5 @@ Template.afFieldInput.helpers({
 
     return templateName;
   },
-  innerContext: function afFieldInputContext() {
-    var c = AutoForm.Utility.getComponentContext(this, "afFieldInput");
-    var form = AutoForm.getCurrentDataForForm();
-    var formId = form.id;
-    var ss = AutoForm.getFormSchema();
-    var defs = c.defs;
-    const instance = Template.instance()
-    const template = AutoForm.templateInstanceForForm();
-
-    // Get schema default value.
-    // We must do this before adjusting defs for arrays.
-    var schemaDefaultValue = defs.defaultValue;
-
-    // Adjust for array fields if necessary
-    if (defs.type === Array) {
-      defs = AutoForm.Utility.getFieldDefinition(ss, c.atts.name + ".$");
-    }
-
-    // Determine what `type` attribute should be if not set
-    var inputType = AutoForm.getInputType(this);
-    var componentDef = AutoForm._inputTypeDefinitions[inputType];
-    if (!componentDef) {
-      throw new Error('AutoForm: No component found for rendering input with type "' + inputType + '"');
-    }
-
-    // Get reactive mDoc
-    var mDoc = AutoForm.reactiveFormData.sourceDoc(formId);
-
-    // Get input value
-    var value = getInputValue(c.atts, c.atts.value, mDoc, schemaDefaultValue, c.atts.defaultValue, componentDef, template);
-
-    // Build input data context
-    var iData = getInputData(defs, c.atts, value, ss.label(c.atts.name), form.type);
-
-    // These are needed for onRendered
-
-    instance.afFieldName = c.atts.name
-    instance.afFieldValue = value
-
-    // Adjust and return context
-    return (typeof componentDef.contextAdjust === "function") ? componentDef.contextAdjust(iData) : iData;
-  }
+  innerContext: afFieldInputContext
 });
