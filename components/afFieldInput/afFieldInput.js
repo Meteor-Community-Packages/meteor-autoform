40b40e8ae6b99acf406511910efb3e4aa8e71561
Jan Küster <jkuester@uni-bremen.de>
2020-11-22 20:12:24 +0100

    components afFieldInput fix missing theme template in rare cases

--- components/afFieldInput/afFieldInput.js
+++ components/afFieldInput/afFieldInput.js
@@ -13,17 +13,16 @@ Template.afFieldInput.onRendered(() => {
 });
 
 export function afFieldInputContext() {
-  var c = AutoForm.Utility.getComponentContext(this, "afFieldInput");
-  var form = AutoForm.getCurrentDataForForm();
-  var formId = form.id;
-  var ss = AutoForm.getFormSchema();
-  var defs = c.defs;
+  const c = AutoForm.Utility.getComponentContext(this, "afFieldInput");
+  const form = AutoForm.getCurrentDataForForm();
+  const formId = form.id;
+  const ss = AutoForm.getFormSchema();
+  let defs = c.defs;
   const instance = Template.instance();
-  const template = AutoForm.templateInstanceForForm();
 
   // Get schema default value.
   // We must do this before adjusting defs for arrays.
-  var schemaDefaultValue = defs.defaultValue;
+  const schemaDefaultValue = defs.defaultValue;
 
   // Adjust for array fields if necessary
   if (defs.type === Array) {
@@ -31,32 +30,27 @@ export function afFieldInputContext() {
   }
 
   // Determine what `type` attribute should be if not set
-  var inputType = AutoForm.getInputType(this);
-  var componentDef = AutoForm._inputTypeDefinitions[inputType];
+  const inputType = AutoForm.getInputType(this);
+  const componentDef = AutoForm._inputTypeDefinitions[inputType];
   if (!componentDef) {
-    throw new Error(
-      'AutoForm: No component found for rendering input with type "' +
-        inputType +
-        '"'
-    );
+    throw new Error(`AutoForm: No component found for rendering input with type "${inputType}"`);
   }
 
   // Get reactive mDoc
-  var mDoc = AutoForm.reactiveFormData.sourceDoc(formId);
+  const mDoc = AutoForm.reactiveFormData.sourceDoc(formId, void 0);
 
   // Get input value
-  var value = getInputValue(
+  const value = getInputValue(
     c.atts,
     c.atts.value,
     mDoc,
     schemaDefaultValue,
     c.atts.defaultValue,
     componentDef,
-    template
   );
 
   // Build input data context
-  var iData = getInputData(
+  const iData = getInputData(
     defs,
     c.atts,
     value,
@@ -79,30 +73,37 @@ Template.afFieldInput.helpers({
   // similar to AutoForm.getTemplateName, but we have fewer layers of fallback, and we fall back
   // lastly to a template without an _ piece at the end
   getTemplateName: function getTemplateName() {
-    var self = this;
-
+    const self = this;
     // Determine what `type` attribute should be if not set
-    var inputType = AutoForm.getInputType(this);
-    var componentDef = AutoForm._inputTypeDefinitions[inputType];
+    const inputType = AutoForm.getInputType(this);
+    const componentDef = AutoForm._inputTypeDefinitions[inputType];
     if (!componentDef) {
-      throw new Error(
-        'AutoForm: No component found for rendering input with type "' +
-          inputType +
-          '"'
-      );
+      throw new Error(`AutoForm: No component found for rendering input with type "${inputType}"`);
     }
 
-    var inputTemplateName = componentDef.template;
-    var styleTemplateName = this.template;
+    const inputTemplateName = componentDef.template;
+    const styleTemplateName = this.template;
 
-    // We skip the check for existence here so that we can get the `_plain` string
-    // even though they don't exist.
-    var templateName = AutoForm.getTemplateName(
+    // on first attempt we try to get the template without skipping non-existent
+    // templates in order to circumvent false-positives that may occur due to
+    // custom data context, set in conten blocks or dynamic templates
+    let templateName = AutoForm.getTemplateName(
       inputTemplateName,
       styleTemplateName,
       self.name,
-      true
+      false
     );
+
+    if (!templateName) {
+      // In case we found nothing, we skip the check for existence here so that
+      // we can get the `_plain` string even though they don't exist.
+      templateName = AutoForm.getTemplateName(
+        inputTemplateName,
+        styleTemplateName,
+        self.name,
+        true
+      );
+    }
 
     // Special case: the built-in "plain" template uses the basic input templates for
     // everything, so if we found _plain, we use inputTemplateName instead
